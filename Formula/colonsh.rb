848052f552674e8ad46224082655790bc9f5bc36
Stephen Baidu <2970017+stephenbaidu@users.noreply.github.com>
2025-11-30 02:09:35 -0800

    fix(v0.0.1): Update repo location

--- Formula/colonsh.rb
+++ Formula/colonsh.rb
@@ -1,8 +1,8 @@
 class Colonsh < Formula
   desc "A command-line alias manager and utility tool written in Go."
-  homepage "https://github.com/colonsh/colonsh" 
+  homepage "https://github.com/stephenbaidu/colonsh" 
   
-  url "https://github.com/colonsh/colonsh/archive/refs/tags/v0.0.1.tar.gz"
+  url "https://github.com/stephenbaidu/colonsh/archive/refs/tags/v0.0.1.tar.gz"
   sha256 "64ffe575adb6f711950ed221caf0a3bb289fa92dafc69e4dbe6ebcf3e4ee783d" 
 
   license "MIT"
